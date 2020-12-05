78175b167b33037796a3429f76bad9773356ebd8
Sean Farley <somethingfarley@gmail.com>
2020-12-04 19:19:48 -0500

    Passport

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,27 +47,24 @@ app.use(session({
   resave: false,
   store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res,next){
-  console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user){
-
-    
+  if(!req.user){    
       const err = new Error('You are not authenticated!');
       err.status = 401;
       return next(err);
     } else { //there is a signed cookie
-    if (req.session.user === 'authenticated'){
+    
       return next(); //authorized
-    } else {
-      const err = new Error('You are not authenticated!' + user + " " + pass);
-      err.status=401;
-      return next(err);
     }
-  }
+  
 }
 
 
--- models/user.js
+++ models/user.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Scheme = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unqiue: true,
-    },
-    password: {
-    type: String,
-    require: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
