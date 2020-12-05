5e1546b28e8d915d523c4ba08e105db6b7e4c00e
Sean Farley <somethingfarley@gmail.com>
2020-12-04 22:30:27 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -4,10 +4,21 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Scheme = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        default: '',
+    
+    },
+    lastname: {
+        type: String,
+        default: '',
+    
+    },
     admin: {
         type: Boolean,
         default: false
     }
+
 });
 
 userSchema.plugin(passportLocalMongoose);
